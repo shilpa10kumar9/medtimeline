923ab7ff799826f64b954ed255f6366def57836b
Shilpa Kumar <shilpakumar@google.com>
2019-03-28 13:34:45 -0400

    padstart polyfill

--- medtimeline/src/app/cardcontainer/cardcontainer.component.ts
+++ medtimeline/src/app/cardcontainer/cardcontainer.component.ts
@@ -90,6 +90,27 @@ export class CardcontainerComponent {
                                 .reduce((acc, val) => acc.concat(val), []);
     this.setUpCards();
     this.setUpDrag(dragulaService);
+    // https://github.com/uxitten/polyfill/blob/master/string.polyfill.js
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/
+    //    Reference/Global_Objects/String/padStart
+    if (!String.prototype.padStart) {
+      String.prototype.padStart = function padStart(targetLength, padString) {
+        padString =
+            String((typeof padString !== 'undefined' ? padString : ' '));
+        if (this.length > targetLength) {
+          return String(this);
+        } else {
+          targetLength = targetLength - this.length;
+          if (targetLength > padString.length) {
+            padString += padString.repeat(
+                targetLength /
+                padString.length);  // append to original to ensure we are
+                                    // longer than needed
+          }
+          return padString.slice(0, targetLength) + String(this);
+        }
+      };
+    }
   }
 
   private setUpCards() {
