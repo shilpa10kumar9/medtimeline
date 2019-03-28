59997a2fa90ecc52b04ba9b9473c8fef195eed4b
Shilpa Kumar <shilpakumar@google.com>
2019-03-28 13:36:06 -0400

    Update Github Pages demo.

--- docs/main.js
+++ docs/main.js
@@ -714,6 +714,27 @@ var CardcontainerComponent = /** @class */ (function () {
             .reduce(function (acc, val) { return acc.concat(val); }, []);
         this.setUpCards();
         this.setUpDrag(dragulaService);
+        // https://github.com/uxitten/polyfill/blob/master/string.polyfill.js
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/
+        //    Reference/Global_Objects/String/padStart
+        if (!String.prototype.padStart) {
+            String.prototype.padStart = function padStart(targetLength, padString) {
+                padString =
+                    String((typeof padString !== 'undefined' ? padString : ' '));
+                if (this.length > targetLength) {
+                    return String(this);
+                }
+                else {
+                    targetLength = targetLength - this.length;
+                    if (targetLength > padString.length) {
+                        padString += padString.repeat(targetLength /
+                            padString.length); // append to original to ensure we are
+                        // longer than needed
+                    }
+                    return padString.slice(0, targetLength) + String(this);
+                }
+            };
+        }
     }
     CardcontainerComponent.prototype.setUpCards = function () {
         // Add a textbox at the top of the card list.
